2140c30f0247df41b001b318e6169a9f97289932
Abhijeet Kasurde <akasurde@redhat.com>
2015-08-26 17:26:35 +0530

    Updated function call
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- pyversion.py
+++ pyversion.py
@@ -1,7 +1,7 @@
 import sys
 
 # Easy way to detect Python Version
-MAJOR, MINOR, _, _, _ = sys.version_info()
+MAJOR, MINOR, _, _, _ = sys.version_info
 
 if MAJOR >= 3 or (MAJOR == 2 and MINOR >= 7):
     print("We are running python3")
