a7fed9ab4a5abc331c8e1175fcd1fad2b01217c2
Abhijeet Kasurde <akasurde@redhat.com>
2015-10-19 21:29:00 +0530

    Day 2 added
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- flask_tuts/app/views.py
+++ flask_tuts/app/views.py
@@ -1,6 +1,18 @@
 from app import app
+from flask import render_template
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello World"
+    user = { 'nickname' : 'Abhijeet'}
+    posts = [ 
+        { 'author' : { 'nickname' : 'Abhijeet'},
+            'body' : ' Sample Text in body 1',
+        },
+        {
+            'author' : {'nickname': 'Aayush'},
+            'body' : 'Sample text in body 2',
+        }
+    ]
+        
+    return render_template('index.html', title='Home', user=user, posts=posts)
