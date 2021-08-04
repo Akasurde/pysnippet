f093a5297b0e85e7ba85db1a52c20650dd635e25
Abhijeet Kasurde <akasurde@redhat.com>
2021-08-04 19:38:22 +0530

    Update decorator
    Signed-off-by: Abhijeet Kasurde <akasurde@redhat.com>

--- python_concepts/example_3.py
+++ python_concepts/example_3.py
@@ -1,12 +1,24 @@
 def is_authenticated(f):
-    def wraps(*args):
+    def wraps(*args, **kwargs):
         print("Checking if user authenticated")
         print("Calling function %s" % f.__name__)
-        return f(args)
+        if f.__name__ == 'update_user':
+            print("Updating user might take a long time")
+            print(kwargs)
+        else:
+            print(args)
+        return f(*args, **kwargs)
     return wraps
 
 @is_authenticated
 def get_user(user_id):
     print("Get User %s" % user_id)
 
+@is_authenticated
+def update_user(user_id, user_data):
+    print("Update User %s" % user_id)
+
 get_user('Abhijeet')
+update_user('Abhijeet', user_data='Savitoj')
+
+
